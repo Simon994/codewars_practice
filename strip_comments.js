569f7c77798cd4594b5a32090bfddc7ae626c22b
Simon994 <65023744+Simon994@users.noreply.github.com>
2020-06-26 15:35:00 +0100

    removing breakdown stages

--- strip_comments.js
+++ strip_comments.js
@@ -11,30 +11,3 @@ function solution(input, markers) {
 };
 
 solution("apples, plums % and bananas\npears\noranges !applesauce", ["%", "!"]);
-
-
-//first, split up string to array, based on new line chars 
-let test = "apples, plums % and bananas\npears\noranges !applesauce"
-let testArr = test.split('\n');
-
-
-
-// 1) for any given string recognise the index of the first char that matches ANY marker char
-let test2 = "apples, plums % and bananas";
-   //need to recognise ANY marker, so using Regex:
-   let test3 = "apples, plums % and ! bananas";
-   let arrTest = ["%","!"];
-   let regexpFromArr = new RegExp (`[${arrTest.join('')}]`);
-   let result3 = regexpFromArr.exec(test3).index   
-   //console.log(result3);
-
-// 2) slice string from index recognised by regex exec match
-   let slicedStr = test3.slice(0, result3);
-   //console.log(slicedStr);
-
-// 3) Strip out any WS at end of line  
-   let endTrimmedSlicedStr = slicedStr.replace(/\s*$/,"")
-   //console.log(endTrimmedSlicedStr);
-
-
-//solution("apples, plums % and bananas\npears\noranges !applesauce", ["%", "!"]);
